47d23740c622602acf19fffd2692aa4b6c7f9cd5
Miles Pool <Miles.pool@7digital.com>
2014-09-25 10:22:58 +0100

    Setup test class

---
+++ CheckoutKataApi.Tests/TestFramework.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CheckoutKataApi.Tests
+{
+    [TestFixture]
+    public class TestFramework
+    {
+        [Test]
+        public void Blargh()
+        {
+            
+        }
+
+    }
+}
