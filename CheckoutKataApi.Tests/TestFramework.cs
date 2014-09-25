ededf5c84994293a9c72010a488b2dd6cc209129
Miles Pool <Miles.pool@7digital.com>
2014-09-25 10:45:15 +0100

    Completed initial http-ok response test

--- CheckoutKataApi.Tests/TestFramework.cs
+++ CheckoutKataApi.Tests/TestFramework.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -11,10 +12,11 @@ namespace CheckoutKataApi.Tests
     public class TestFramework
     {
         [Test]
-        public void Blargh()
+        public void Get_ok_response_from_http_request()
         {
-            
+            var webRequest = WebRequest.Create("http://checkout-kata-api.local/");
+            var htmlRequest = (HttpWebResponse) webRequest.GetResponse();
+            Assert.That(htmlRequest.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
-
     }
 }
